d2da0088591d2f75d97f7d40fe8ba601fa540910
Mariamreema <mariamreema34@gmail.com>
2025-01-06 16:36:13 +0530

    feat: server pofrt updated

--- src/components/SurveyForm.js
+++ src/components/SurveyForm.js
@@ -20,7 +20,7 @@ const SurveyForm = () => {
 
   const handleSubmit = async () => {
     try {
-        const response = await axios.post('https://backend-ochre-one-61.vercel.app/', formData);
+        const response = await axios.post('https://backend-ochre-one-61.vercel.app/api/surveys', formData);
         console.log('Survey submitted:', response.data);
         alert("Yay!!Your survey has been submitted")
     } catch (error) {
